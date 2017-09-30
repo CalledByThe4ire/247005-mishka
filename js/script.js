ce1ad0e32e53202a6cb7dc5e0881a5303ecc54f5
amurtuzov <flock1992@yandex.ru>
2017-09-30 19:49:38 +0300

    Sass styles

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ window.initMap = function() {
   });
 
         // Маркер
-  var image = '';
+  var image = 'img/icon-map-pin.svg';
   var marker = new google.maps.Marker({
     map: map,
     position: myLatLng,
@@ -22,20 +22,36 @@ window.initMap = function() {
 }
 
 
-// // slider
-// var sliderArray = new Array(document.querySelector("#slide_1"),document.querySelector("#slide_2"),document.querySelector("#slide_3"));
-// sliderArray[1].style.display="none";
-// sliderArray[2].style.display="none";
-// var currentSlide = 1;
-// function nextSlide() {
-//   currentSlide++;
-//   if(currentSlide === sliderArray.length)
-//   currentSlide = 1;
-//       sliderArray[currentSlide].style.display="block";
-//       sliderArray[currentSlide--].style.display="none";
+// slider with add/remove style
+
+var slider = document.querySelector('.slider');
+var slides = slider.querySelectorAll('blockquote');
+var currentSlide = 0;
+slides[1].style.display = "none";
+slides[2].style.display = "none";
+function slideToggle(toggle) {
+  slides[currentSlide].style.display = "none";
+  currentSlide += toggle;
+  if(slides.length === currentSlide) currentSlide = 0;
+  if(currentSlide < 0) currentSlide = slides.length - 1;
+  slides[currentSlide].style.display = "block";;
+};
+slider.querySelector('.slider__toggle--next').onclick = function() {slideToggle(1)};
+slider.querySelector('.slider__toggle--prev').onclick = function() {slideToggle(-1)};
+
+
+// slider with class add/remove
+
+// var slider = document.querySelector('.slider');
+// var slides = slider.querySelectorAll('blockquote');
+// var currentSlide = 0;
+// function slideToggle(toggle) {
+//   slides[currentSlide].classList.remove('show-slide');
+//   currentSlide += toggle;
+//    if(slides.length === currentSlide) currentSlide = 0;
+//    if(currentSlide < 0) currentSlide = slides.length - 1;
+//    slides[currentSlide].classList.add('show-slide');
 // };
 
-// document.querySelector(".reviews__paginator-next").onclick = nextSlide;
-
-
-
+// slider.querySelector('.slider__toggle--next').onclick = function() {slideToggle(1)};
+// slider.querySelector('.slider__toggle--prev').onclick = function() {slideToggle(-1)};
